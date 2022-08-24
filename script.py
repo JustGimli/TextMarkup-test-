c652c86c526154d3a3c5875c009c370f13b8bf92
JustGimli <aleksnermer@gmail.com>
2022-08-24 16:43:03 +0300

    set algorithm

---
+++ script.py
@@ -0,0 +1,37 @@
+
+def open_sourse():
+    list_eng = []
+    list_rus = []
+    with open('sourse.txt') as t:
+        
+        for line in t:
+            
+            list_line = line.split('\t')
+            eng = list_line[0].split(';')
+            rus = list_line[1].split(';')
+
+            for i in range(len(eng)):
+
+                word_eng = eng[i].strip()
+                
+                list_eng.append(word_eng)
+                
+
+            for i in range(len(rus)):
+                word_rus = rus[i].strip('\n').strip()
+                list_rus.append(word_rus)
+
+            main(list(set(list_eng)), list(set(list_rus)))
+            list_eng.clear()
+            list_rus.clear()
+
+def main(list_eng,list_rus):
+
+    for i in range(len(list_eng)):
+
+        for j in range(len(list_rus)):
+
+            print(list_eng[i], list_rus[j])
+
+
+open_sourse()
